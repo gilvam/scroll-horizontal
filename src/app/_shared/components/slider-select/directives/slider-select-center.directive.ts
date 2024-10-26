3b55a1ef770375752b7e38d9b9bdb3332532341a
Gilvam Mourao <gilvamm@gmail.com>
2024-10-26 15:23:21 -0300

    feat: slide-select init

--- src/app/_shared/components/slider-select/directives/slider-select-center.directive.ts
+++ src/app/_shared/components/slider-select/directives/slider-select-center.directive.ts
@@ -1,20 +1,79 @@
-import { AfterViewInit, Directive, ElementRef, Renderer2 } from '@angular/core';
+import {
+	AfterViewInit,
+	Directive,
+	ElementRef,
+	EventEmitter,
+	HostListener,
+	Input,
+	OnChanges,
+	OnDestroy,
+	Output,
+	Renderer2,
+	SimpleChanges
+} from '@angular/core';
+import { fromEvent, merge, retry, Subscription, switchMap, take, timer } from 'rxjs';
 
 @Directive({
 	selector: '[appSliderSelectCenter]',
 	standalone: true
 })
-export class SliderSelectCenterDirective implements AfterViewInit {
+export class SliderSelectCenterDirective implements OnChanges, OnDestroy, AfterViewInit {
+	@Input() select = 0;
+	@Output() selectChange = new EventEmitter<number>();
+	private elList: Element[] = Array.from(this.el.nativeElement.querySelectorAll('.gallery-item')) || [];
+	private time = 500;
+	private takeMax = 20;
+	private timerSubscription!: Subscription;
+
 	constructor(
 		private el: ElementRef,
 		private renderer: Renderer2
 	) {}
 
 	ngAfterViewInit(): void {
-		const list: HTMLCollection = this.el.nativeElement.children;
-		console.log(`el: `, list);
-		console.log(`el: `, this.el.nativeElement);
+		this.observeScroll();
+	}
 
-		// this.renderer.setStyle(this.el.nativeElement, 'left', `${this.el.nativeElement.clientWidth / 2}px`);
+	ngOnChanges(changes: SimpleChanges): void {
+		// this.observeClear();
+	}
+
+	ngOnDestroy(): void {
+		this.observeClear();
+	}
+
+	private observeScroll(): void {
+		const events$ = [fromEvent(this.el.nativeElement, 'touchend')];
+		this.timerSubscription = merge(...events$)
+			.pipe(
+				switchMap(() => timer(0, this.time)),
+				take(this.takeMax)
+			)
+			.subscribe(() => this.emitSelected());
+	}
+
+	private observeClear(): void {
+		if (!this.timerSubscription) {
+			return;
+		}
+		this.timerSubscription.unsubscribe();
+	}
+
+	private emitSelected(): void {
+		console.log('emitSelected');
+		this.selectChange.emit(this.choiceItem);
+	}
+
+	get choiceItem(): number {
+		const widthPerImage = (this.el.nativeElement.scrollWidth - this.el.nativeElement.clientWidth) / this.elList.length;
+		const widthInTheMoment = this.el.nativeElement.scrollLeft;
+		return widthPerImage < widthInTheMoment ? 1 : 0;
+	}
+
+	@HostListener('mouseup')
+	@HostListener('touchend')
+	@HostListener('touchcancel')
+	forceListenerOnSafari(): void {
+		this.observeScroll();
 	}
 }
--- src/app/_shared/components/slider-select/slider-select.component.ts
+++ src/app/_shared/components/slider-select/slider-select.component.ts
@@ -1,6 +1,4 @@
-import { AfterViewInit, Component, ElementRef, HostListener, Inject, ViewChild } from '@angular/core';
-import { DOCUMENT } from '@angular/common';
-import { NumberUtil } from '../../utils/number.util';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { SliderSelectCenterDirective } from './directives/slider-select-center.directive';
 
 @Component({
@@ -10,71 +8,18 @@ import { SliderSelectCenterDirective } from './directives/slider-select-center.d
 	templateUrl: './slider-select.component.html',
 	styleUrl: './slider-select.component.scss'
 })
-export class SliderSelectComponent implements AfterViewInit {
+export class SliderSelectComponent {
 	@ViewChild('gallery', { static: true }) gallery!: ElementRef<HTMLDivElement>;
-	images: string[] = [`https://picsum.photos/${401}/500`, `https://picsum.photos/${402}/500`];
+	// `https://picsum.photos/400/500`,
+	images: string[] = [
+		'https://fastly.picsum.photos/id/794/401/500.jpg?hmac=Oy1UuAwEqLCdqzWJ7Ui4EwhcVFwP0_bBQTtVAnUGRX4',
+		'https://fastly.picsum.photos/id/323/400/500.jpg?hmac=OwqqXxIyaOWAYJHtJKMMganeCNN6Oj9ydMV3NVq1Ot0'
+	];
 	selected = 0;
-	private style!: CSSStyleDeclaration;
-	private width = 0;
-	private scrollLeftPos = 0;
-	private padding = 0;
-	private gap = 0;
 
-	constructor(@Inject(DOCUMENT) private document: Document) {}
-
-	ngAfterViewInit(): void {
-		this.getStyled();
-	}
-
-	private getStyled(): void {
-		this.width = this.gallery.nativeElement.clientWidth;
-		this.scrollLeftPos = this.gallery.nativeElement.scrollLeft;
-		this.style = this.document.defaultView!.getComputedStyle(this.gallery.nativeElement);
-		this.padding = NumberUtil.onlyNumber(this.style.paddingLeft);
-		this.gap = NumberUtil.onlyNumber(this.style.gap);
-	}
+	constructor() {}
 
 	select(item: number): void {
 		this.selected = item;
-
-		if (this.selected) {
-			this.scrollLeft();
-			return;
-		}
-		this.scrollRight();
-	}
-
-	scrollLeft(): void {
-		let newScroll = this.scrollLeftPos - this.width + this.padding + this.gap * 4;
-		if (newScroll < 0) {
-			newScroll = this.gallery.nativeElement.scrollWidth - this.gallery.nativeElement.clientWidth;
-		}
-		this.gallery.nativeElement.scroll({ left: newScroll, behavior: 'smooth' });
-	}
-
-	scrollRight(): void {
-		let newScroll = this.scrollLeftPos + this.width - this.padding - this.gap * 4;
-		if (this.scrollLeftPos + this.width >= this.gallery.nativeElement.scrollWidth) {
-			newScroll = 0;
-		}
-		this.gallery.nativeElement.scroll({ left: newScroll, behavior: 'smooth' });
-	}
-
-	private centerNearestImage(): void {
-		const scrollLeft = this.gallery.nativeElement.scrollLeft;
-		const itemWidth = this.width + this.gap;
-		const nearestIndex = Math.round(scrollLeft / itemWidth);
-		const nearestPosition = nearestIndex * itemWidth;
-
-		console.log(`nearestPosition: `, nearestPosition);
-
-		this.gallery.nativeElement.scrollTo({ left: nearestPosition, behavior: 'smooth' });
-	}
-
-	@HostListener('mouseup')
-	@HostListener('touchend')
-	@HostListener('touchcancel')
-	onMouseUp(): void {
-		this.centerNearestImage();
 	}
 }
