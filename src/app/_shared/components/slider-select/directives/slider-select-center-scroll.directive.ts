5aecb4cc9ea116fce465d19d0b88abfffe9a61f4
Gilvam Mourao <gilvamm@gmail.com>
2024-10-28 10:16:05 -0300

    feat: scroll mobile on

--- src/app/_shared/components/slider-select/directives/slider-select-center-scroll.directive.ts
+++ src/app/_shared/components/slider-select/directives/slider-select-center-scroll.directive.ts
@@ -1,23 +1,43 @@
-import { AfterViewInit, Directive, ElementRef, EventEmitter, HostListener, Input, OnDestroy, Output } from '@angular/core';
-import { debounceTime, distinctUntilChanged, fromEvent, Subscription } from 'rxjs';
+import {
+	AfterViewInit,
+	Directive,
+	ElementRef,
+	EventEmitter,
+	HostListener,
+	Input,
+	OnChanges,
+	OnDestroy,
+	Output,
+	Renderer2,
+	SimpleChanges
+} from '@angular/core';
+import { debounceTime, distinctUntilChanged, fromEvent, Subscription, timer } from 'rxjs';
 
 @Directive({
 	selector: '[appSliderSelectCenterScroll]',
 	standalone: true
 })
-export class SliderSelectCenterScrollDirective implements OnDestroy, AfterViewInit {
+export class SliderSelectCenterScrollDirective implements OnDestroy, AfterViewInit, OnChanges {
 	@Input() appSliderSelectCenterScroll = 0;
 	@Output() appSliderSelectCenterScrollChange = new EventEmitter<number>();
-	private elListQuerySelector = '.gallery-item';
+	private elListQuerySelector = 'img';
 	private elList: Element[] = [];
 	private time = 30;
 	private timerSubscription!: Subscription;
 
-	constructor(private el: ElementRef) {}
+	constructor(
+		private el: ElementRef,
+		private renderer: Renderer2
+	) {}
 
 	ngAfterViewInit(): void {
 		this.updateList();
+		this.setPadding();
 		this.onScroll();
+	}
+
+	ngOnChanges(changes: SimpleChanges): void {
+		this.changeByButton(changes);
 	}
 
 	ngOnDestroy(): void {
@@ -49,10 +69,53 @@ export class SliderSelectCenterScrollDirective implements OnDestroy, AfterViewIn
 		this.elList = Array.from(this.el.nativeElement.querySelectorAll(this.elListQuerySelector)) || [];
 	}
 
+	@HostListener('window:resize', ['$event'])
+	onResize(): void {
+		this.setPadding();
+	}
+
+	setPadding(): void {
+		this.renderer.setStyle(this.el.nativeElement, 'padding', `0`);
+		this.renderer.setStyle(this.el.nativeElement, 'gap', `0`);
+
+		timer(this.time).subscribe(() => {
+			const firstImage = this.elList[0];
+			const screenWidth = this.el.nativeElement.closest('body').clientWidth;
+			const padding = (screenWidth - firstImage.scrollWidth) / 2;
+			const gap = screenWidth - (padding + firstImage.scrollWidth) - 40;
+
+			this.renderer.setStyle(this.el.nativeElement, 'padding', `0 ${padding}px`);
+			this.renderer.setStyle(this.el.nativeElement, 'gap', `${gap}px`);
+		});
+	}
+
 	clear(): void {
 		if (!this.timerSubscription) {
 			return;
 		}
 		this.timerSubscription.unsubscribe();
+	}
+
+	changeByButton(changes: SimpleChanges): void {
+		const { select } = changes;
+		if (select?.firstChange) {
+			return;
+		}
+
+		const distance = this.appSliderSelectCenterScroll ? this.scrollableWidth : 0;
+		this.setScrollLeft(distance);
+	}
+
+	setScrollLeft(distance = 0): void {
+		const property = 'scrollLeft';
+		const scrollLeft = Math.ceil(this.el.nativeElement.scrollLeft);
+
+		if (this.appSliderSelectCenterScroll === 0 && scrollLeft) {
+			this.renderer.setProperty(this.el.nativeElement, property, distance);
+		}
+
+		if (this.appSliderSelectCenterScroll === 1 && !scrollLeft) {
+			this.renderer.setProperty(this.el.nativeElement, property, distance);
+		}
 	}
 }
--- src/app/_shared/components/slider-select/slider-select.component.ts
+++ src/app/_shared/components/slider-select/slider-select.component.ts
@@ -1,18 +1,16 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { SliderSelectCenterScrollDirective } from './directives/slider-select-center-scroll.directive';
-import { SliderSelectCenterSelectDirective } from './directives/slider-select-center-select.directive';
 
 @Component({
 	selector: 'app-slider-select',
 	standalone: true,
-	imports: [SliderSelectCenterScrollDirective, SliderSelectCenterSelectDirective],
+	imports: [SliderSelectCenterScrollDirective],
 	templateUrl: './slider-select.component.html',
 	styleUrl: './slider-select.component.scss'
 })
 export class SliderSelectComponent {
-	@ViewChild('gallery', { static: true }) gallery!: ElementRef<HTMLDivElement>;
-	// `https://picsum.photos/400/500`,
 	images: string[] = [
+		// `https://picsum.photos/400/500`,
 		'https://fastly.picsum.photos/id/794/401/500.jpg?hmac=Oy1UuAwEqLCdqzWJ7Ui4EwhcVFwP0_bBQTtVAnUGRX4',
 		'https://fastly.picsum.photos/id/323/400/500.jpg?hmac=OwqqXxIyaOWAYJHtJKMMganeCNN6Oj9ydMV3NVq1Ot0'
 	];
